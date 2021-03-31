d6414155a114c40c330467a8102b204e097a3909
meenakshi <meenakshimanirudhan@gmail.com>
2021-03-31 09:35:19 +0530

    my first commit

---
+++ javascriptprograms/languagefundamentals/languagefundamentals.js
@@ -0,0 +1,10 @@
+/*var company="luminar technolab";
+console.log("our company name is"+ company)*/
+
+//var age=25;
+//var name="varun";
+//console.log("hai am " + name +" "+ age + "years old")
+
+var company="luminar technolab";
+var location="kakkanad";
+console.log(`our company is ${company} is located at ${location}`);
